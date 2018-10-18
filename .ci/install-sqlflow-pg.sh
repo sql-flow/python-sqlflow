81315e8551132f79af965a1d47c8c98cb497da9b
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-10-18 23:30:11 +0200

    Fix

--- .ci/install-sqlflow-pg.sh
+++ .ci/install-sqlflow-pg.sh
@@ -12,7 +12,11 @@ tar -zxf master.tar.gz
 
 ls -l
 
+echo "Create user and database"
+psql -c "CREATE USER myflow WITH PASSWORD 'myflow';" -U postgres
+psql -c 'CREATE DATABASE myflow WITH OWNER myflow;' -U postgres
+
 echo "Install SQLFlow on PostgreSQL"
 cd pg-extension-master
-psql -v "ON_ERROR_STOP=1" -f ./sql/sqlflow-structure.sql -U myflow myflow
-psql -v "ON_ERROR_STOP=1" -f ./sql/sqlflow-instance.sql -U myflow myflow
+psql -v "ON_ERROR_STOP=1" -f ./sql/sqlflow-structure.sql -U postgres myflow
+psql -v "ON_ERROR_STOP=1" -f ./sql/sqlflow-instance.sql -U postgres myflow
